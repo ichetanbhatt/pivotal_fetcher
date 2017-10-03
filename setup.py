e0fcf2065340eb709781c676401757da36122b7c
Chetan <ichetanbhatt@gmail.com>
2017-10-03 17:16:01 +0530

    pip initialized

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pivotal_fetcher',
+      version='0.1',
+      description='Fetches PivotalTracker Stories',
+      url='http://github.com/xxihawkxx/pivotal_fetcher',
+      author='Chetan Bhatt',
+      author_email='ichetanbhatt@gmail.com',
+      license='',
+      packages=[''],
+      zip_safe=False)

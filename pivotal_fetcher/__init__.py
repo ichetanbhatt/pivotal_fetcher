940da8a9dfdfb2121f176520d6ab037fbc57a352
Chetan <ichetanbhatt@gmail.com>
2017-10-10 13:38:56 +0530

    Final Changes

--- pivotal_fetcher/__init__.py
+++ pivotal_fetcher/__init__.py
@@ -8,10 +8,11 @@ with open(file_path, "r") as ins:
     for line in ins:
         array.append(line)
 
+
 # Extract APIKEY and Owner
 token = (array[0].split('=', 1)[1])[0:-1]
 u_name = array[2].split('=', 1)[1]
-
+states = ['Accepted', 'Delivered', 'Finished', 'Started']
 
 # Accepted Stories
 def accepted():
@@ -25,48 +26,40 @@ def accepted():
             print "Please define how old data you want!"
             return
         else:
-            headers = {'X-TrackerToken': ''+token}
-            hit_url = "https://www.pivotaltracker.com/services/v5/projects/" + p_id + "/search?"
-            req_date = int(sys.argv[1])
-            query_date = datetime.today() - timedelta(days=req_date)
-            # Accepted Stories GET req to API
-            r = requests.get(hit_url+'query=created_since:"'+str(query_date.day)+'-'+str(query_date.month)+'-' +
-                             str(query_date.year) +
-                             '" and owner:"'+u_name+'" and state:accepted', headers=headers)
-            res_json = ast.literal_eval(r.text).get('stories').get('stories')
-            a_lt = len(res_json)
             print "[" + p_name + "]"
-            print "Accepted"
-            for x in range(a_lt):
-                a_result = res_json[x]
-                story = a_result.get('name')
-                story_type = a_result.get('story_type')
-                state = a_result.get('current_state')
-                estimate = str(a_result.get('estimate'))
-                # Change Date Format
-                created_date = datetime.strptime((a_result.get('created_at').split('T')[0]), "%Y-%m-%d")
-                created_at = datetime.strftime(created_date, "%d-%b-%Y")
-                accepted_date = datetime.strptime((a_result.get('accepted_at').split('T')[0]), "%Y-%m-%d")
-                accepted_at = datetime.strftime(accepted_date, "%d-%b-%Y")
+            for state in states:
+                headers = {'X-TrackerToken': ''+token}
+                hit_url = "https://www.pivotaltracker.com/services/v5/projects/" + p_id + "/search?"
+                req_date = int(sys.argv[1])
+                query_date = datetime.today() - timedelta(days=req_date)
+                query = hit_url + 'query=owner:"'+u_name+'" and state:' + state
+                # Accepted Stories GET req to API
+                if state is 'Accepted':
+                    query = hit_url+'query=accepted_since:"'+str(query_date.day)+'-'+str(query_date.month)+\
+                            '-' +str(query_date.year) +'" and owner:"'+u_name+'" and state:' + state
 
-                owners = str(a_result.get('owner_ids'))
-                table = "Story:[" + story + "] Type:[" + story_type + "] Estimate:[" + estimate + "] Created:[" + \
-                        created_at + "] Accepted:[" + accepted_at + "] Owners:" + owners
-                print table
-            # Other Stories
-            o = requests.get(hit_url + 'query=-state:accepted AND owner:"'+u_name+'"', headers=headers)
-            o_res_json = ast.literal_eval(o.text).get('stories').get('stories')
-            o_lt = len(o_res_json)
-            print ""
-            print "Started"
-            for x in range(o_lt):
-                result = o_res_json[x]
-                story = result.get('name')
-                story_type = result.get('story_type')
-                created_date = datetime.strptime((result.get('created_at').split('T')[0]), "%Y-%m-%d")
-                created_at = datetime.strftime(created_date, "%d-%b-%Y")
-                owners = str(result.get('owner_ids'))
-                table = "Story:[" + story + "] Type:[" + story_type + "] Created:[" + created_at + "] Owners:" + owners
-                print table
-            print ""
+                response = requests.get(query, headers=headers)
+                res_json = ast.literal_eval(response.text).get('stories').get('stories')
+                lt = len(res_json)
 
+                print state
+                for x in range(lt):
+                    result = res_json[x]
+                    story = result.get('name')
+                    story_type = result.get('story_type')
+                    estimate = str(result.get('estimate'))
+                    # Change Date Format
+                    created_date = datetime.strptime((result.get('created_at').split('T')[0]), "%Y-%m-%d")
+                    created_at = datetime.strftime(created_date, "%d-%b-%Y")
+                    accepted_date = result.get('accepted_at')
+                    if accepted_date is not None:
+                        accepted_date = datetime.strptime((result.get('accepted_at').split('T')[0]), "%Y-%m-%d")
+                        accepted_at = datetime.strftime(accepted_date, "%d-%b-%Y")
+                    else:
+                        accepted_at = "NA"
+                    table = story + " - " + story_type + " Estimate:[" + estimate + "] Created:[" \
+                            + created_at + "] Accepted:[" + accepted_at + "]"
+                    print table
+                print ""
+
+accepted()
